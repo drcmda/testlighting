bfd041f921ac4433682c7a267a2967717df1f944
Paul Henschel <drcmda@gmail.com>
2020-02-19 01:06:51 +0100

    remove controls

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { Suspense, useMemo, useRef, useState, useEffect } from 'react'
-import { Canvas, Dom, useThree, useFrame, extend, useLoader } from 'react-three-fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Canvas, Dom } from 'react-three-fiber'
 import lerp from 'lerp'
 import Model from './Model'
 import Post from './Post'
-
-extend({ OrbitControls })
-const Controls = props => {
-  const { gl, camera } = useThree()
-  const ref = useRef()
-  useFrame(() => ref.current.update())
-  return <orbitControls ref={ref} args={[camera, gl.domElement]} {...props} />
-}
 
 export default function App() {
   return (
@@ -19,7 +10,6 @@ export default function App() {
       <Canvas shadowMap camera={{ position: [0, 0, 12], fov: 50, near: 7, far: 15 }}>
         <fog attach="fog" args={['black', 0, 20]} />
         <pointLight position={[0, 10, -10]} intensity={1} />
-        <Controls />
         <Suspense
           fallback={
             <Dom center className="loader">
