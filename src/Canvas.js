355ea09f2d0eb32f62e387c4aedc839faf71d9ca
Paul Henschel <drcmda@gmail.com>
2020-02-20 00:50:41 +0100

    create split bundle

---
+++ src/Canvas.js
@@ -0,0 +1,21 @@
+import React, { Suspense, useMemo, useRef, useState, useEffect } from 'react'
+import { Canvas, Dom } from 'react-three-fiber'
+import lerp from 'lerp'
+import Model from './Model'
+
+export default function App() {
+  return (
+    <Canvas shadowMap camera={{ position: [0, 0, 12], fov: 50, near: 7, far: 15 }}>
+      <fog attach="fog" args={['black', 0, 20]} />
+      <pointLight position={[0, 10, -10]} intensity={1} />
+      <Suspense
+        fallback={
+          <Dom center className="loader">
+            LOADING
+          </Dom>
+        }>
+        <Model position={[0, -6, 0]} rotation={[0, -0.2, 0]} />
+      </Suspense>
+    </Canvas>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import App from './App'
+import { render } from 'react-dom'
+import React, { Suspense, lazy } from 'react'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Create a lazy-loaded split bundle for the canvas
+const Canvas = lazy(() => import('./Canvas'))
+
+render(
+  <>
+    <Suspense fallback={null}>
+      <Canvas />
+    </Suspense>
+    <div class="header">
+      <span class="active">ART</span>
+      <span>ABOUT</span>
+      <span>VISIT</span>
+      <span>SHOP</span>
+      <span>SEARCH</span>
+    </div>
+    <div class="middle">
+      <h2>The</h2>
+      <h1>THREE GRACES</h1>
+    </div>
+  </>,
+  document.getElementById('root'),
+)
